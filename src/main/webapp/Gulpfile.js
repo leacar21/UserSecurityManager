92d2e7b5c04b80bd86e7d98351bde24255c58192
Leandro Carrasco <carrascoleandro@MacBookLea.local>
2015-04-14 22:52:19 -0300

    * Avance

--- src/main/webapp/Gulpfile.js
+++ src/main/webapp/Gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp'),
-    connect = require('gulp-connect');
+var gulp = require('gulp');
+var connect = require('gulp-connect');
 
 // Servidor web de desarrollo
 gulp.task('server', function() {
@@ -10,8 +10,8 @@ gulp.task('server', function() {
   });
 });
 
-var stylus = require('gulp-stylus'),
-    nib = require('nib');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
 
 // Pre-procesa archivos Stylus a CSS y recarga los cambios
 gulp.task('css', function() {
@@ -27,8 +27,8 @@ gulp.task('html', function() {
     .pipe(connect.reload());
 });
 
-var jshint  = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+var jshint  = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 // Busca errores en el JS y nos los muestra por pantalla
 gulp.task('jshint', function() {
@@ -38,12 +38,38 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('fail'));
 });
 
+// Busca en las carpetas de estilos y javascript los archivos que hayamos creado
+// para inyectarlos en el index.html
+var inject  = require('gulp-inject');
+var wiredep = require('wiredep').stream;
+
+gulp.task('inject', function() {
+  var sources = gulp.src([ './app/scripts/**/*.js', './app/stylesheets/**/*.css' ]);
+  return gulp.src('index.html', { cwd: './app' })
+    .pipe(inject(sources, {
+      read: false,
+      ignorePath: '/app'
+    }))
+    .pipe(gulp.dest('./app'));
+});
+// Inyecta las librerias que instalemos vía Bower
+gulp.task('wiredep', function () {
+  gulp.src('./app/index.html')
+    .pipe(wiredep({
+      directory: './app/lib'
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
 // Vigila cambios que se produzcan en el código
 // y lanza las tareas relacionadas
 gulp.task('watch', function() {
   gulp.watch(['./app/**/*.html'], ['html']);
   gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
   gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint']);
+  gulp.watch(['./app/stylesheets/**/*.styl'], ['css', 'inject']);
+  gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
+  gulp.watch(['./bower.json'], ['wiredep']);
 });
 
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
---
+++ src/main/webapp/app/scripts/app.js
@@ -0,0 +1,39 @@
+(function () {
+  'use strict';
+
+  //------------------------------------------------------
+
+  angular.module('blog', ['ngRoute', 'blog.controllers']);
+
+  //------------------------------------------------------
+
+  function config ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/post-list.tpl.html',
+        controller: 'PostListController',
+        controllerAs: 'postlist'
+      })
+      .when('/post/:postId', {
+        templateUrl: 'views/post-detail.tpl.html',
+        controller: 'PostDetailController',
+        controllerAs: 'postdetail'
+      })
+      .when('/new', {
+        templateUrl: 'views/post-create.tpl.html',
+        controller: 'PostCreateController',
+        controllerAs: 'postcreate'
+      });
+  }
+
+  //------------------------------------------------------
+
+  angular
+    .module('blog')
+    .config(config);
+
+  //------------------------------------------------------
+
+})();
