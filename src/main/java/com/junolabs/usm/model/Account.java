944286de5387f3674195e3b7a77559f7fe60aace
Leandro Carrasco <carrascoleandro@MacBookLea.local>
2015-05-01 18:43:34 -0300

    *

--- src/main/java/com/junolabs/usm/model/Account.java
+++ src/main/java/com/junolabs/usm/model/Account.java
@@ -7,37 +7,46 @@ public class Account extends GenericEntity {
 	private String password;
 	private User user;
 	private List<Role> roles;
-	private List<Permission> permisioms;
-	
+	private List<Permission> permissions;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
 	public List<Role> getRoles() {
 		return roles;
 	}
+
 	public void setRoles(List<Role> roles) {
 		this.roles = roles;
 	}
-	public List<Permission> getPermisioms() {
-		return permisioms;
+
+	public List<Permission> getPermissions() {
+		return permissions;
 	}
-	public void setPermisioms(List<Permission> permisioms) {
-		this.permisioms = permisioms;
+
+	public void setPermissions(List<Permission> permisioms) {
+		this.permissions = permisioms;
 	}
 
 }
